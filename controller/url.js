7646202eb9d5e518b54672d4e977c1575e4b8030
yash gondaliya <yashgondaliya666@gmail.com>
2024-08-08 12:51:32 +0530

    using date.now()

--- controller/url.js
+++ controller/url.js
@@ -1,9 +1,10 @@
-const shortid = require("shortid");
+// const shortid = require("shortid");
 const URL = require("../models/user");
 
 async function generateShotURL(req, res) {
   const body = req.body;
-  const shortId = shortid();
+  // const shortId = shortid();
+  const shortId = Date.now();
 
   const data = new URL({
     shortId,
