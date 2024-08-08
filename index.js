650739344097dee1b55aa937af1e35e160caf2bf
yash gondaliya <yashgondaliya666@gmail.com>
2024-08-08 19:14:19 +0530

    ejs concept

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-// const express = require("express");
-// const connectionToDB = require("./db/connection");
-// const urlRoute = require("./routes/url");
-// const URL = require("./models/user");
-// const app = express();
-
-// app.use(express.json());
-
-// app.get("/", (_, res) => {
-//   res.send("Home Page");
-// });
-
-// app.use("/url", urlRoute);
-
-// app.get("/:id", async (req, res) => {
-//   const shortId = req.params.id;
-//   const data = await URL.findOne({ shortId });
-//   console.log("data", data);
-//   res.redirect(302, data.originalURL);
-// });
-
-// app.listen(8001, () => {
-//   connectionToDB();
-//   console.log("Server is running on port 8001");
-// });
-
 const express = require("express");
 const connectionToDB = require("./db/connection");
 const urlRoute = require("./routes/url");
 const URL = require("./models/user");
 const app = express();
+const path = require("path");
+const staticRouter = require("./routes/staticRouter");
 
 app.use(express.json());
 
@@ -36,13 +12,18 @@ app.get("/", (_, res) => {
   res.send("Home Page");
 });
 
-app.use("/url", urlRoute);
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-// app.get("/:id", async (req, res) => {
-//   const shortId = req.params.id;
-//   const data = await URL.findOne({ shortId });
-//   res.redirect(302, data.originalURL);
-// });
+app.get("/ejs", async (_, res) => {
+  const allUrl = await URL.find({});
+  return res.render("home", {
+    urls: allUrl,
+  });
+});
+
+app.use("/url", urlRoute);
+app.use("/", staticRouter);
 
 app.get("/:id", async (req, res) => {
   try {
@@ -50,14 +31,11 @@ app.get("/:id", async (req, res) => {
     const data = await URL.findOne({ shortId });
 
     if (!data) {
-      // Handle the case where the shortId is not found
       return res.status(404).send("URL not found");
     }
 
-    // Redirect to the original URL
     res.redirect(302, data.originalURL);
   } catch (error) {
-    // Handle potential errors from the database query
     console.error("Error finding URL:", error);
     res.status(500).send("Internal Server Error");
   }
