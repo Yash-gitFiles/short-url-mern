5ff20ff7130f1165f76e4e7f3b5855b58d4618f2
yash gondaliya <yashgondaliya666@gmail.com>
2024-08-09 12:14:51 +0530

    complete

--- index.js
+++ index.js
@@ -7,39 +7,42 @@ const path = require("path");
 const staticRouter = require("./routes/staticRouter");
 
 app.use(express.json());
-
-app.get("/", (_, res) => {
-  res.send("Home Page");
-});
-
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
-
-app.get("/ejs", async (_, res) => {
-  const allUrl = await URL.find({});
-  return res.render("home", {
-    urls: allUrl,
-  });
-});
-
+app.use(express.urlencoded({ extended: false })); // form-data support karne ke liye.
 app.use("/url", urlRoute);
-app.use("/", staticRouter);
+app.use("/static", staticRouter);
+
+// app.get("/", (_, res) => {
+//   return res.send("Home Page");
+// });
 
 app.get("/:id", async (req, res) => {
   try {
     const shortId = req.params.id;
+    console.log("shortId", shortId);
     const data = await URL.findOne({ shortId });
 
-    if (!data) {
-      return res.status(404).send("URL not found");
-    }
+    console.log(":", data);
 
-    res.redirect(302, data.originalURL);
+    if (!data || data === null) {
+      return res.status(404).send("URL not found");
+    } else {
+      return res.redirect(302, data.originalURL);
+    }
   } catch (error) {
     console.error("Error finding URL:", error);
     res.status(500).send("Internal Server Error");
   }
 });
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// app.get("/ejs", async (_, res) => {
+//   const allUrl = await URL.find({});
+//   return res.render("home", {
+//     urls: allUrl,
+//   });
+// });
 
 app.listen(8001, () => {
   connectionToDB();
